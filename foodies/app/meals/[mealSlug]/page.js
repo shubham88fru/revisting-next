70e79745ca2ae79cf08c0881a5b804e75ff5489f
shubham88fru <shubham88fru@yahoo.com>
2025-01-30 17:07:44 -0700

    add pages

---
+++ foodies/app/meals/[mealSlug]/page.js
@@ -0,0 +1,5 @@
+function MealSlugPage() {
+  return <h1>Meal Slug Page</h1>;
+}
+
+export default MealSlugPage;
---
+++ foodies/app/meals/share/page.js
@@ -0,0 +1,5 @@
+function SharePage() {
+  return <h1>Share Meal</h1>;
+}
+
+export default SharePage;
