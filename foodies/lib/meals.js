cd418955b3d793e69f9a7748b9d99cafa53a2424
shubham88fru <shubham88fru@yahoo.com>
2025-02-08 15:35:30 -0700

    meal details page

--- foodies/lib/meals.js
+++ foodies/lib/meals.js
@@ -5,3 +5,7 @@ const db = sql("meals.db");
 export function getAllMeals() {
   return db.prepare("SELECT * FROM meals").all();
 }
+
+export async function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
